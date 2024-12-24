6eb0a0d16fc66db4d79b452075335583c8d3e7e8
diefei0001 <tearasbutterfly@gmail.com>
2024-12-24 17:32:37 +0800

    12 /24 17:32:51

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
+import turtle
 
+def draw_polygon(sides, size):
+    angle = 360 / sides
+    for _ in range(sides):
+        turtle.forward(size)
+        turtle.right(angle)
 
+# 绘制一个旋转的多边形
+for rotation in range(0, 360, 30):  # 每次旋转 30 度
+    turtle.seth(rotation)
+    draw_polygon(6, 50)  # 六边形
+
+turtle.done()
 
      
 
