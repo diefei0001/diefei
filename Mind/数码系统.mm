d56b1916f81b31691e17650c33ad70d3f1c9ce74
diefei0001 <tearasbutterfly@gmail.com>
2025-03-09 12:15:17 +0800

    计算机科学导论

---
+++ Mind/数码系统.mm
@@ -0,0 +1,100 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="数码系统" FOLDED="false" ID="ID_107805750" CREATED="1741329192501" MODIFIED="1741329213455" BACKGROUND_COLOR="#0099ff" STYLE="narrow_hexagon" UNIFORM_SHAPE="true" MAX_WIDTH="55.83837 pt" MIN_WIDTH="55.83837 pt" VGAP_QUANTITY="12.75 pt">
+<font NAME="华文仿宋" SIZE="10"/>
+<hook NAME="MapStyle" zoom="0.9090909">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff" show_note_icons="true" associatedTemplateLocation="template:/BigMap.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_1273250224" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="CENTER" MAX_WIDTH="90 pt" MIN_WIDTH="90 pt" VGAP_QUANTITY="2 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="LINEAR_PATH" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1273250224" STARTINCLINATION="100.8 pt;0 pt;" ENDINCLINATION="100.8 pt;6.6 pt;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+<font NAME="Arial" SIZE="9" BOLD="true" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" TEXT_ALIGN="LEFT">
+<font SIZE="11" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="9" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#33ff00" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1358928635">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#0000ff" TRANSPARENCY="255" DESTINATION="ID_1358928635"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" UNIFORM_SHAPE="true" MAX_WIDTH="120 pt" MIN_WIDTH="120 pt">
+<font SIZE="24" ITALIC="true"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
+<node TEXT="数码" POSITION="bottom_or_right" ID="ID_449897166" CREATED="1741329213871" MODIFIED="1741329289557" VSHIFT_QUANTITY="-13.8 pt">
+<font NAME="华文仿宋"/>
+<node TEXT="用于表示数字的符号" ID="ID_467476272" CREATED="1741329309124" MODIFIED="1741329519951">
+<font NAME="华文仿宋"/>
+</node>
+</node>
+<node TEXT="位置化数码系统" POSITION="bottom_or_right" ID="ID_1587296940" CREATED="1741329248100" MODIFIED="1741330290752" HGAP_QUANTITY="30.8 pt" VSHIFT_QUANTITY="29.4 pt">
+<font NAME="华文仿宋"/>
+<node TEXT="数码被赋予位置量" ID="ID_1460904854" CREATED="1741329328816" MODIFIED="1741329519986">
+<font NAME="华文仿宋"/>
+</node>
+<node TEXT="数码的位置固定" ID="ID_1243693716" CREATED="1741329382997" MODIFIED="1741329519990">
+<font NAME="华文仿宋"/>
+</node>
+</node>
+<node TEXT="数的抽象" POSITION="top_or_left" ID="ID_507892511" CREATED="1741329297448" MODIFIED="1741329639839" HGAP_QUANTITY="-164.8 pt" VSHIFT_QUANTITY="61.8 pt" VGAP_QUANTITY="2 pt">
+<font NAME="华文仿宋"/>
+<node TEXT="S（k-1）···S2 S1 S0。S-1S-2S（-l）" ID="ID_1199607152" CREATED="1741329426450" MODIFIED="1741329519992" HGAP_QUANTITY="-200.80001 pt" VSHIFT_QUANTITY="3 pt">
+<font NAME="华文仿宋"/>
+</node>
+</node>
+<node TEXT="位权展开式" POSITION="top_or_left" ID="ID_1055028410" CREATED="1741329582480" MODIFIED="1741330776083" HGAP_QUANTITY="-172.6 pt" VSHIFT_QUANTITY="18.6 pt" VGAP_QUANTITY="2 pt">
+<font NAME="华文仿宋"/>
+<node TEXT="位权展开式" ID="ID_269776080" CREATED="1741329626342" MODIFIED="1741330776078" HGAP_QUANTITY="-260.80001 pt" VSHIFT_QUANTITY="0.6 pt">
+<hook URI="../../Pictures/Screenshots/屏幕截图%202025-03-07%20144514.png" SIZE="0.340643" NAME="ExternalObject"/>
+<font NAME="华文仿宋"/>
+</node>
+</node>
+<node TEXT="进制" POSITION="bottom_or_right" ID="ID_1186057805" CREATED="1741330276701" MODIFIED="1741330761827" HGAP_QUANTITY="-207.40001 pt" VSHIFT_QUANTITY="-61.2 pt" VGAP_QUANTITY="2 pt">
+<font NAME="华文仿宋"/>
+<node TEXT="定义了每个数位的符号范围" LOCALIZED_STYLE_REF="styles.important" ID="ID_1153063717" CREATED="1741330327400" MODIFIED="1741330410112" HGAP_QUANTITY="-202.60001 pt" VSHIFT_QUANTITY="-21.6 pt"/>
+<node TEXT="进制最大值" ID="ID_296453454" CREATED="1741330459306" MODIFIED="1741330761798" HGAP_QUANTITY="-163.6 pt" VSHIFT_QUANTITY="11.4 pt" VGAP_QUANTITY="2 pt">
+<font NAME="华文仿宋"/>
+<node TEXT="b^k-1" ID="ID_283233739" CREATED="1741330470471" MODIFIED="1741330486137" HGAP_QUANTITY="-182.2 pt" VSHIFT_QUANTITY="-13.8 pt"/>
+</node>
+<node TEXT="进制转换最小值" ID="ID_1938902411" CREATED="1741330569837" MODIFIED="1741330761805" HGAP_QUANTITY="-130 pt" VSHIFT_QUANTITY="37.8 pt" VGAP_QUANTITY="2 pt">
+<font NAME="华文仿宋"/>
+<node TEXT="任意小于b的k次方的数，可以被以b为基底的k个数码表示" ID="ID_1134726361" CREATED="1741330602349" MODIFIED="1741330676931" HGAP_QUANTITY="-197.80001 pt" VSHIFT_QUANTITY="-21.6 pt"/>
+</node>
+</node>
+</node>
+</map>
