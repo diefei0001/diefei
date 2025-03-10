6679b00a2332c2ed7bc83b5913f3f4c7e7300f1a
diefei0001 <tearasbutterfly@gmail.com>
2025-03-10 11:43:11 +0800

    云计算技术概论

---
+++ Mind/云计算概念、技术、架构/云计算起源与发展.mm
@@ -0,0 +1,109 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="云计算起源与影响" FOLDED="false" ID="ID_107805750" CREATED="1741575111318" MODIFIED="1741575526082" BACKGROUND_COLOR="#0099ff" STYLE="narrow_hexagon" UNIFORM_SHAPE="true" MAX_WIDTH="55.83837 pt" MIN_WIDTH="55.83837 pt" VGAP_QUANTITY="12.75 pt">
+<font SIZE="10"/>
+<hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff" show_note_icons="true" associatedTemplateLocation="template:/BigMap.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_1273250224" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="CENTER" MAX_WIDTH="90 pt" MIN_WIDTH="90 pt" VGAP_QUANTITY="2 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="LINEAR_PATH" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1273250224" STARTINCLINATION="100.8 pt;0 pt;" ENDINCLINATION="100.8 pt;6.6 pt;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+<font NAME="Arial" SIZE="9" BOLD="true" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" TEXT_ALIGN="LEFT">
+<font SIZE="11" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="9" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#33ff00" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1358928635">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#0000ff" TRANSPARENCY="255" DESTINATION="ID_1358928635"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" UNIFORM_SHAPE="true" MAX_WIDTH="120 pt" MIN_WIDTH="120 pt">
+<font SIZE="24" ITALIC="true"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
+<node TEXT="云计算定义" POSITION="bottom_or_right" ID="ID_197967435" CREATED="1741575120577" MODIFIED="1741577749531">
+<font NAME="华文仿宋"/>
+<node TEXT="分布式计算的一种形式，它引入效用模型远程提供可拓展可测量的资源" ID="ID_205868776" CREATED="1741575742574" MODIFIED="1741575824579"/>
+</node>
+<node TEXT="云计算发展的商业驱动力" POSITION="top_or_left" ID="ID_1047007180" CREATED="1741575187158" MODIFIED="1741577715232">
+<font NAME="华文仿宋"/>
+<node TEXT="" ID="ID_1667073711" CREATED="1741575716719" MODIFIED="1741575716719">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="IT资源的容量规划" ID="ID_1175936686" CREATED="1741575206276" MODIFIED="1741577715205">
+<font NAME="华文仿宋"/>
+</node>
+<node TEXT="IT资源的成本问题" ID="ID_900671312" CREATED="1741575263697" MODIFIED="1741577715232">
+<font NAME="华文仿宋"/>
+</node>
+<node TEXT="企业变化对IT资源的动态需求" ID="ID_666682686" CREATED="1741575278102" MODIFIED="1741576074031"/>
+<node TEXT="" ID="ID_1523385136" CREATED="1741575716719" MODIFIED="1741575716719">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="成本问题" ID="ID_732308384" CREATED="1741575716725" MODIFIED="1741577699422">
+<font NAME="华文仿宋"/>
+</node>
+</node>
+</node>
+<node TEXT="云计算的前期技术" POSITION="bottom_or_right" ID="ID_238811254" CREATED="1741575296932" MODIFIED="1741577760192">
+<font NAME="华文仿宋"/>
+<node TEXT="集群化技术" ID="ID_722950772" CREATED="1741575318350" MODIFIED="1741577760160">
+<font NAME="华文仿宋"/>
+</node>
+<node TEXT="网格计算" ID="ID_1827838739" CREATED="1741575329825" MODIFIED="1741577760189">
+<font NAME="华文仿宋"/>
+</node>
+<node TEXT="虚拟化技术" ID="ID_1096409365" CREATED="1741575370563" MODIFIED="1741577760189">
+<font NAME="华文仿宋"/>
+</node>
+<node TEXT="云使能技术" ID="ID_1904432424" CREATED="1741575379397" MODIFIED="1741577760192">
+<font NAME="华文仿宋"/>
+</node>
+</node>
+<node TEXT="起源与发展" POSITION="top_or_left" ID="ID_1291983683" CREATED="1741575492505" MODIFIED="1741577741121">
+<font NAME="华文仿宋"/>
+<node TEXT="Salesforce.com  引入提供远程服务的概念" ID="ID_1578000059" CREATED="1741575581602" MODIFIED="1741575626307"/>
+<node TEXT="Amazon Web服务" ID="ID_1711916651" CREATED="1741575628667" MODIFIED="1741577732346">
+<font NAME="华文仿宋"/>
+</node>
+<node TEXT="Amazon 弹性服务云" ID="ID_1450807281" CREATED="1741575673087" MODIFIED="1741577732351">
+<font NAME="华文仿宋"/>
+</node>
+</node>
+</node>
+</map>
