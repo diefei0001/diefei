b0bdd3305ef4d904a68c3355057774648e32e447
diefei0001 <tearasbutterfly@gmail.com>
2025-05-27 22:17:52 +0800

    new

--- Coding/My C/paper.C
+++ Coding/My C/paper.C
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    double num1 = 1.0;
-    double num2 =2.0;
-    double num3 =num1+num2;
-    printf("%f",num3);
+    char apple;
+    apple = '\040';
+    printf("%c",apple);
 }
