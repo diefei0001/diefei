fbd6692e56c11775dcfacdc861ba071873f44f74
diefei0001 <tearasbutterfly@gmail.com>
2025-06-28 19:23:09 +0800

    new

--- TechnologyBook/C/C programs in book/paper.C
+++ TechnologyBook/C/C programs in book/paper.C
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-    char new_str[40] = " to day "
-"is a good " "day";
-    printf("%s",new_str);
-
+    int a ,b ,c ,d ,e ,f ,result;
+    bool g = true;
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+    e = 5;
+    f = 6;
+    result=a + b > c * d && e != f || !g;
+    printf("%d",result);
 }
